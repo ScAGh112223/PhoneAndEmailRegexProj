1bc18d1635a2208f414bb5546991c38f2ce0bfc2
Curious-Junior <shlokbrar@gmail.com>
2023-03-29 18:59:11 -0700

    polished spreadsheet a little and made init file to manage dependencies

---
+++ __init__.py
@@ -0,0 +1,25 @@
+import importlib # Used to reliably import dependecies as variables
+import sys, re # Import dependecies that dont need to be or cannot be installed
+
+# __all__ allows the variables or module names in it to be accessed from any file that import __init__.*
+__all__ = ["sys", "re"] # Initialize __all__ with sys as it is needed for both init and main.py and re to just only have one import in main
+dependecies = ["pyperclip", "pprint", "requests", "signal", "uuid", "readchar", "xlsxwriter", "pathlib"] # List of dependecies
+
+for dependyName in dependecies: # Go through needed dependecies
+    try: # try except to check if package is already installed (to save on load times)
+        # make a vairable with the name of the dependecy which is needed as __all__ only accepts variable names and that name is how you access that dependecy.
+        # Example: if i set the name to "dep" and install XlsxWriter, i would need to use "dep" to access it in main.py
+        exec(f"{dependyName} = importlib.import_module('{dependyName}')")
+        __all__.append(dependyName) # Append dependecy to __all__
+    except ImportError: # If package is not installed
+        try: # Try except in case installation fails
+            # Run pip install and disable stdout as well as stderr so that the user doesn't see pip progress
+            subprocess.check_call([sys.executable, "-m", "pip", "install", dependyName],
+                                  stdout=subprocess.DEVNULL,
+                                  stderr=subprocess.STDOUT)
+
+            exec(f"{dependyName} = importlib.import_module('{dependyName}')") # Install dependecy (explained in line 10 and 11)
+            __all__.append(dependyName) # Append dependecy to __all__
+        except Exception as e:
+            print(f"Failed to update/install {dependyName}.\nExiting program. {e}") # Print and error message
+            sys.exit(-1) # Exit with error code
--- main.py
+++ main.py
@@ -1,16 +1,17 @@
-import re
-import readline # Regex for email and phone number detection
-import sys, signal # Sys and signal for disabling the Traceback after Ctrl+C from displaying
-import installDependencies as id # Custom script that installs pip packages
-import pprint as pp
-import uuid
+#import pathlib # Used to get current woking directory
+#import re # Regex for email and phone number detection
+#import sys, signal # Sys and signal for disabling the Traceback after Ctrl+C from displaying
+from __init__ import *# Custom script that installs pip packages
+#import pprint as pp # Print data in a nice format
+#import uuid # Generate unique identifier for filenames
 
-id.install(["pyperclip", "requests", "XlsxWriter", "readchar"]) # Run install command in case pyperclip and requests are not properly installed
+packages = ["re", "sys", "pyperclip", "pprint", "requests", "signal", "uuid", "readchar", "XlsxWriter", "pathlib"]
+ # Run install command in case pyperclip and requests are not properly installed
 
-import pyperclip # Import after install commad
-import requests # Import after install command
-import xlsxwriter # Import after install
-import readchar
+#import pyperclip # Import after install commad
+#import requests # Import after install command
+#import xlsxwriter # Import after install
+#import readchar
 
 # Detecting phone numbers
 def find_PhoneNumbers(i: str):
@@ -42,25 +43,52 @@ def find_Email(i: str):
 
   return res # Return found emails
 
-def save_To_Sheet(d: list):
-  book = xlsxwriter.Workbook(f"PaEDd-{uuid.uuid4()}.xlsx")
-  sheet = book.add_worksheet()
+# Function that saved requested data to xlsx file
+def save_To_Sheet(data: list):
+  '''Writes given data to a uniquely named file.
+
+  @params:
+      data - data to write to file(must be enumerable)
+  @returns:
+      Tuple - (name of file, full path to file)
+  '''
+  uName = f"PaED-{uuid.uuid4()}.xlsx" # Make unique filename (PaED stands for Phone and Email detector)
+  book = xlsxwriter.Workbook(uName) # Initiate xlsx workbook with unique filename.
+  sheet = book.add_worksheet() # Make worksheet(spreadsheet) in workbook
   
-  for r, val in enumerate(d):
-    sheet.write(r+1, 0, f"Index {r}")
-    sheet.write(r+1, 1, val)
+  for r, val in enumerate(data): # Loop through data and get indeces with enumerate()
+    sheet.write(r+1, 0, r) # Index each data entry  
+    sheet.write(r+1, 1, val) # Add value for each entry
   
-  sheet.write(0, 0, f"Email index ({len(d)} in total)")
-  sheet.write(0, 1, f"Email address")
+  sheet.write(0, 0, f"Email index ({len(data)} in total)") # Add Index column title and total number of entries
+  sheet.write(0, 1, f"Email address") # Add title for value column
 
-  book.close()
+  book.close() # Close filestream
+  
+  return (uName, f"{pathlib.Path().resolve()}/{uName}")
 
-def exit_interface(dat):
-  print("Do you wish to save all extracted phones and emails of this session as a spreadsheet?(y/n)? ", end="")
-  sv = readchar.readchar().strip().lower()
+# SIGINT(Ctrl+C) handler function to ask user if they want to save data collected in session
+def exit_interface(data: list):
+  '''Provides an interface for the user to optionally save all data extracted within session to a xlsx file.
 
-  if (sv == "y"): save_To_Sheet(dat)
-  sys.exit(0)
+  @params:
+    data - List with all phone numbers and emails to save.
+  @returns:
+    nothing.
+  '''
+  # Need to use end = "" to prevent the cursor from going to the next line. Can't use input() in a SIGINT handler.
+  # Flush to remove buffer as it messes up code sequence when in SIGINT handler function.
+  print("\nDo you wish to save all extracted phones and emails of this session as a spreadsheet?(y/n)? ", end="", flush=True)
+  sv = readchar.readkey().strip().lower() # Read using readchar.readkey() as input() does not work.
+
+  if (sv == "y"): # If the user wants to, save data to file
+      filePath = save_To_Sheet(data)[1] # Write file and get filePath
+      print(f"\nPath to file: {filePath}\nDo you wish to copy this path?(y/n) ", end="") # Tell user file path and ask them if they want to copy it
+      cp = readchar.readkey().strip().lower() # Take input
+      if(cp == "y"): # If user wants to copy file path
+          pyperclip.copy(filePath) # Copy filePath
+          print("Path copied!") # Inform user that the path has been copied
+  sys.exit(0) # End program
 
 # Function that nicely prints detected emails and phone numbers
 # Made into a function to avoid repeating it 3 times
@@ -79,15 +107,18 @@ def output_Interface(i: str):
         if res: 
           # Format all phone numbers with commas using list comprehension, join them into a string and remove the first redundant comma and space using [2:]
           numbers = ''.join([f", {p}" for p in res])[2:]
-          print(f"Phone number(s) found: {numbers}") # Print numbers
+
+          print("Phone number(s) found: ")
+          pprint.pprint(numbers) # Print numbers
           
           cp = input("Do you wish to copy the detected phone numbers to your clipboard?(y/n)").lower().strip() # Ask if the user wants to copy results
           if cp == "y": 
             pyperclip.copy(numbers) # Copy results to user's clipboard
             print("Result Copied!") # Inform user that the results have been copied
           return res
-        else: 
+        else:
           print("No phone numbers found.") # In case nothing is found
+          return [] # Return empty list if nothin is found
     elif F == 'e':
         res = find_Email(i) # find emails
         
@@ -95,7 +126,8 @@ def output_Interface(i: str):
           # Format all emails with commas using list comprehension, join them into a string and remove the first redundant comma and space using [2:]
           emails = ''.join([f", {p}" for p in res])[2:] 
 
-          pp.pprint(f"Email(s) found: {emails}") # Print emails
+          print("Email(s) found: ")
+          pprint.pprint(emails) # Print emails
 
           cp = input("Do you wish to copy the detected emails to your clipboard?(y/n)").lower().strip() # Ask if the user wants to copy results
           if cp == "y": 
@@ -104,6 +136,7 @@ def output_Interface(i: str):
           return res
         else: 
           print("No emails found.") # In case nothing is found
+          return [] # Return empty list in case nothing is found
 
 # Only ask for input if file is being ran directly so that it can be used as a module
 if (__name__ == "__main__"):
@@ -112,7 +145,7 @@ if (__name__ == "__main__"):
   print("Options for input type: \n1.Terminal\n2.File\n3.Website")
 
   totalSessionData = []
-  signal.signal(signal.SIGINT, lambda x,y : exec("exit_interface(dat=totalSessionData)"))
+  signal.signal(signal.SIGINT, lambda x,y : exec("exit_interface(data=totalSessionData)"))
 
   while True:
     FT = input("Enter your input type of choice: ").strip().lower() # Ask for input type
@@ -121,7 +154,7 @@ if (__name__ == "__main__"):
     ### INPUT TYPE ONE
     if (FT == "1"):
         i = input("Enter data: ").strip().lower()
-        totalSessionData.append(output_Interface(i))
+        totalSessionData.extend(output_Interface(i))
 
     ### INPUT TYPE TWO
     if (FT == "2"):
@@ -129,7 +162,7 @@ if (__name__ == "__main__"):
       try: # Try except in case file does not exist
         with open(filename, "r") as file: # Open file
           i = file.read() # Read file data
-          totalSessionData.append(output_Interface(i)) # Show interface with data read from file
+          totalSessionData.extend(output_Interface(i)) # Show interface with data read from file
       except FileNotFoundError:
           print("File not found, try again.") # Inform user that thier file could not be found
 
@@ -140,6 +173,6 @@ if (__name__ == "__main__"):
         # Send a get request to the url, which returns the a http response with the contents of the website.
         # Then access the http response's content with .content and after that, decode the bytes into a string for processing.
         i = requests.get(url).content.decode("utf-8")
-        totalSessionData.append(output_Interface(i)) # Show interface with data read from website
+        totalSessionData.extend(output_Interface(i)) # Show interface with data read from website
       except requests.RequestException:
         print("Invalid URL") # Inform user that the URL is invalid
